c0f339014ebdde6674e7d2761a6dbeefba58c5e8
melodiesdev <melodiesdev@proton.me>
2024-05-20 13:48:32 +1000

    feat: initial commit

---
+++ src/main/java/dev/melodies/ReptopiaClient.java
@@ -0,0 +1,10 @@
+package dev.melodies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReptopiaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/dev/melodies/ReptopiaDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.melodies;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ReptopiaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
